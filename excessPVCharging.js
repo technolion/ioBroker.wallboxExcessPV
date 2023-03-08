9d155bfbc82fe9b4cf01acd5a91cc7b97b734d7c
Thomas Verbeek <tv@skalio.com>
2023-03-08 16:43:05 +0100

    Update excessPVCharging.js
    improvements

--- excessPVCharging.js
+++ excessPVCharging.js
@@ -8,32 +8,31 @@ const maxAmpere = 16;
 var threePhase = false; //start with one-phase charging
 var list  = new Array(numberSamples);
 var excessPower = 0;
-
-//calculate excess power every second
-schedule('* * * * * *', calcExcess);
-
-//check if charging speed needs to be modified every 2 minutes
-schedule('*/2 * * * *', setWallbox);
+var voltage = 0;
+var wallboxMode = 0;
+var p1 = 0, p2=0, p3 = 0;
 
 currentChargingPower = 0;
 list.fill(getCurrentSurplus());
 
 //initialize in case wallbox is already charging when script is started
-if (getState("easee.0.EH8XMF8C.status.chargerOpMode").val == 3) {
-    var voltage = getState('easee.0.EH8XMF8C.status.voltage').val;
-    currentChargingPower = getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP1").val * voltage
-        + getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP2").val * voltage
-        + getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP3").val * voltage
-    
-    if(currentChargingPower > 3800) {
-        threePhase = true;
-    }
+fetchWallboxInfos();
+if(p1 > 0 && p2> 0 && p3 > 0) {
+    threePhase = true;
+} else {
+    threePhase = false;
 }
+
+//calculate excess power every second
+schedule('* * * * * *', calcSurplus);
+
+//check if charging speed needs to be modified every 2 minutes
+schedule('*/2 * * * *', setWallbox);
 
 console.log("Started PV excess power wallbox charging script. Updates to the wallbox will be attempted every 2 Minutes. Please stand by...");
 
 
-function calcExcess() {
+function calcSurplus() {
     //calculate if we we have surplus or shortage
     var currentSurplus = getCurrentSurplus();
     
@@ -43,15 +42,12 @@ function calcExcess() {
         return a + b;
     });
     avgSurplusPower = Math.round(sum/numberSamples);
-
-    excessPower = Math.round(currentChargingPower + avgSurplusPower);
-
-    //console.log("Excess power: "+excessPower+" W");
+    console.debug("Avg surplus: "+avgSurplusPower+" W", );
 }
 
 function setWallbox() {
-    //chargerOpMode = Offline: 0, Disconnected: 1, AwaitingStart: 2, Charging: 3, Completed: 4, Error: 5, ReadyToCharge: 6
-    var wallboxMode = getState("easee.0.EH8XMF8C.status.chargerOpMode").val;
+    fetchWallboxInfos();
+    excessPower = Math.round(currentChargingPower + avgSurplusPower);
     console.log("Checking if charging speed needs to be adapted.")
     if(excessPower > 2000) {
         // if there is more that 2 kW excess power we start charging
@@ -73,20 +69,17 @@ function setWallbox() {
             return;
         }
 
-        var voltage = getState('easee.0.EH8XMF8C.status.voltage').val;
-
         if(threePhase) {
             //charging on all 3 phases
             var ampere = Math.min(Math.round((excessPower/3)/voltage), maxAmpere);
             ampere = Math.max(ampere, minAmpere);
 
-            if((voltage * ampere * 3) != currentChargingPower) {
-                currentChargingPower = Math.round(voltage * ampere * 3);
+            if(ampere != p1 || ampere != p2 || ampere != p3) {
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP1", ampere);
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP2", ampere);
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP3", ampere);
 
-                console.log('Three-Phase charging now at '+ampere+' Ampere ('+ (currentChargingPower/1000).toFixed(1) +' kW)');
+                console.log('Set Three-Phase charging with '+ampere+' Ampere');
                 //set list to 0 to reset avg access
                 list.fill(0);
             }
@@ -101,14 +94,13 @@ function setWallbox() {
             var ampere = Math.min(Math.round(excessPower/voltage), maxAmpere);
             ampere = Math.max(ampere, minAmpere);
             
-            if((voltage * ampere) != currentChargingPower) {
-                currentChargingPower =  Math.round(voltage * ampere);
+            if(ampere != p1) {
 
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP1", ampere);
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP2", 0);
                 setState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP3", 0);
 
-                console.log('One-Phase charging now at '+ampere+' Ampere ('+ (currentChargingPower/1000).toFixed(1) +' kW)');
+                console.log('Set One-Phase charging with '+ampere+' Ampere');
                 //set list to 0 to reset avg access
                 list.fill(0);
             }
@@ -125,8 +117,6 @@ function setWallbox() {
             setState("easee.0.EH8XMF8C.control.resume", true);
             setState("easee.0.EH8XMF8C.control.start", true);
         }
-
-        console.log('Charging at '+(currentChargingPower/1000).toFixed(1) +' kW, excess power at: ' + excessPower + " W.");
         
     } else {
         console.log('Not charging, excess power too low: ' + excessPower + " W.");
@@ -145,4 +135,39 @@ function setWallbox() {
 
 function getCurrentSurplus() {
     return getState('javascript.0.PV.surplus').val - reservedExcess - getState('javascript.0.PV.regard').val;
+}
+
+function fetchWallboxInfos() {
+    wallboxMode = getState("easee.0.EH8XMF8C.status.chargerOpMode").val;
+    voltage = getState('easee.0.EH8XMF8C.status.voltage').val;
+    p1 = getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP1").val;
+    p2 = getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP2").val;
+    p3 = getState("easee.0.EH8XMF8C.config.dynamicCircuitCurrentP3").val;
+    currentChargingPower = Math.round((p1 * voltage) + (p2 * voltage) + (p3 * voltage));
+    log("Wallbox state is: "+ getWallboxStateString(wallboxMode));
+    log("Voltage is at: "+voltage);
+    log("Phase current is: "+p1+", "+p2+", "+p3);
+    log("Charging power is set at: "+(currentChargingPower/1000).toFixed(1) +" kW)");
+}
+
+function getWallboxStateString(mode) {
+    //chargerOpMode = Offline: 0, Disconnected: 1, AwaitingStart: 2, Charging: 3, Completed: 4, Error: 5, ReadyToCharge: 6
+    switch(mode) {
+        case 0:
+            return "Offline";
+        case 1:
+            return "Disconnected";
+        case 2:
+            return "Awaiting start";
+        case 3:
+            return "Charging";
+        case 4:
+            return "Completed";
+        case 5:
+            return "Error";
+        case 6:
+            return "Ready to charge";
+        default:
+            return "Unknown state "+mode;
+    }
 }
